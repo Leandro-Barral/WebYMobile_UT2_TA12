b4d7518066a2bf01428ff3c6b4f86fddd5bd1ffa
Leandro Barral <leandro.barral@correo.ucu.edu.uy>
2024-08-14 13:12:33 -0300

    UT2_TA12

---
+++ script.js
@@ -0,0 +1,31 @@
+function generatePassword(length) {
+    if (length < 8) {
+        console.log("La contraseña debe tener al menos 8 caracteres.");
+        return;
+    }
+
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+    const allChars = upperCase + lowerCase + numbers + specialChars;
+    let password = "";
+
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += specialChars[Math.floor(Math.random() * specialChars.length)];
+
+    for (let i = password.length; i < length; i++) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    password = password.split('').sort(() => 0.5 - Math.random()).join('');
+    console.log(password);
+    return password;
+}
+
+generatePassword(8);
+generatePassword(12);
+generatePassword(2);
+generatePassword(9999);
